9e69715cd67030aa7554dbf28e50b32183e1309d
Igor <jaknikig@mail.ru>
2023-03-25 17:35:49 +0600

    init

---
+++ task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+//  сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = 4;
+int n = 8;
+Console.WriteLine(SummNumbers(m, n));
+
+int SummNumbers(int m, int n)
+{
+    if(m == n) return m;
+    m += SummNumbers(m + 1, n);
+    return m;
+}
---
+++ task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int m = 2;
+int n = 4;
+int summ = 0;
+
+Console.WriteLine(Akkerman(m, n));
+
+int Akkerman(int m, int n)
+{
+    if(summ > 2400000) return summ;
+    if(m == 0) summ = n + 1;
+    if(m > 0 && n  == 0) 
+    {
+        summ = Akkerman(m - 1, 1);
+    }
+    if(m > 0 && n > 0)
+    {
+        summ = Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+    return summ;
+
+}
